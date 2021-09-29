fd15dd67b4ddfb5114d2f691f32b070d525d765a
Yen Ka <yenkaphu96pnv@gmail.com>
2021-09-29 20:30:37 +0700

    [Admin]: Implement course management

--- src/Redux/Reducers/CourseReducer.js
+++ src/Redux/Reducers/CourseReducer.js
@@ -1,7 +1,18 @@
-const initialState = {};
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        default: return {...state };
-    }
+import { actionCourseTypes } from "../Actions/Types/CourseType";
+
+const initialState = {
+  courseList: [],
 };
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionCourseTypes.SET_COURSE_LIST:
+      state.courseList = action.payload;
+      return { ...state };
+
+    default:
+      return { ...state };
+  }
+};
+
 export default reducer;
--- src/Redux/storeConfig.js
+++ src/Redux/storeConfig.js
@@ -1,11 +1,14 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
-import user from "./Reducers/UserReducer";
 
-// import các reducer con vào
+import user from "./Reducers/UserReducer";
+import course from "./Reducers/CourseReducer";
+
 const reducer = combineReducers({
     user,
+    course
 });
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(
     reducer,
